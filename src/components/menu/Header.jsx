0a80efcedf17d1ed7510a723f590559d51a8b2ec
Shakir Hanif <shakirhanif@hotmail.com>
2023-01-15 21:46:11 +0500

    header complete

--- src/components/menu/Header.jsx
+++ src/components/menu/Header.jsx
@@ -1,23 +1,44 @@
 import { Box, styled } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 
 const Header = () => {
   const account = useSelector((state) => state.account.accountState);
   return (
     <Box
       sx={{
+        display: "flex",
+        justifyContent: "space-between",
         height: "44px",
         background: "#ededed",
         paddingX: "8px",
-        paddngY: "16px",
+        paddingY: "8px",
       }}
     >
       <img
         src={account.picture}
         alt="dp"
-        style={{ height: "40px", width: "40px", borderRadius: "50%" }}
+        style={{
+          height: "40px",
+          width: "40px",
+          borderRadius: "50%",
+          border: "1px solid grey",
+        }}
       />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <DonutLargeIcon sx={{ marginRight: "10px" }} />
+        <ChatIcon sx={{ marginX: "10px", paddingTop: "2px" }} />
+        <MoreVertIcon sx={{ marginLeft: "8px" }} />
+      </Box>
     </Box>
   );
 };
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import React from "react";
 import Header from "./Header";
+import Search from "./Search";
 
 const Menu = () => {
   return (
     <div>
       <Box>
         <Header />
+        <Search />
       </Box>
     </div>
   );
