2bea7859c509b7a66b0dbb0b84c02dcff4c9e50b
Shakir Hanif <shakirhanif@hotmail.com>
2023-01-16 04:09:18 +0500

    infodrawer setup

--- src/components/chat/menu/Header.jsx
+++ src/components/chat/menu/Header.jsx
@@ -1,45 +1,59 @@
-import { Box, styled } from "@mui/material";
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ChatIcon from "@mui/icons-material/Chat";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import HeaderMenu from "./HeaderMenu";
+import InfoDrawer from "../../drawer/InfoDrawer";
 
 const Header = () => {
   const account = useSelector((state) => state.account.accountState);
+  const [openDrawer, setOpenDrawer] = useState("false");
+  const toggleDrawer = () => {
+    setOpenDrawer(true);
+  };
+  const closeDrawer = () => {
+    setOpenDrawer(false);
+  };
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        height: "44px",
-        background: "#ededed",
-        paddingX: "8px",
-        paddingY: "8px",
-      }}
-    >
-      <img
-        src={account.picture}
-        alt="dp"
-        style={{
-          height: "40px",
-          width: "40px",
-          borderRadius: "50%",
-          border: "1px solid grey",
-        }}
-      />
+    <>
       <Box
         sx={{
           display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
+          justifyContent: "space-between",
+          height: "44px",
+          background: "#ededed",
+          paddingX: "8px",
+          paddingY: "8px",
         }}
       >
-        <DonutLargeIcon sx={{ marginRight: "10px" }} />
-        <ChatIcon sx={{ marginX: "10px", paddingTop: "2px" }} />
-        <HeaderMenu sx={{ marginLeft: "8px" }} />
+        <img
+          src={account.picture}
+          alt="dp"
+          style={{
+            height: "40px",
+            width: "40px",
+            borderRadius: "50%",
+            border: "1px solid grey",
+          }}
+          onClick={toggleDrawer}
+          className="hover:cursor-pointer"
+        />
+
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <DonutLargeIcon sx={{ marginRight: "10px" }} />
+          <ChatIcon sx={{ marginX: "10px", paddingTop: "2px" }} />
+          <HeaderMenu sx={{ marginLeft: "8px" }} />
+        </Box>
       </Box>
-    </Box>
+      <InfoDrawer open={openDrawer} closeDrawer={closeDrawer} />
+    </>
   );
 };
 
